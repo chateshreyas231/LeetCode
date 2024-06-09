9c190e8a69e7ff2b7414e8171ebbe7ac57a3cb94
Shreyas Chate <chateshreyas231@gmail.com>
2024-06-09 16:55:27 -0500

    Greatest Common Divisor of Strings

---
+++ LC75/1071. Greatest Common Divisor of Strings/GCD.py
@@ -0,0 +1,15 @@
+class GCD:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        gcd_length = gcd(len(str1), len(str2))
+        return str1[:gcd_length]
+
+sol = GCD()
+print(sol.gcdOfStrings("ABCABC", "ABC"))  
+print(sol.gcdOfStrings("ABABAB", "ABAB")) 
+print(sol.gcdOfStrings("LEET", "CODE"))   
